b465420d45c65f225761843062ec9e9fea472c5c
Lionel SMETS <lionel.smets@isen.yncrea.fr>
2022-10-21 14:49:10 +0200

    app nav

--- app/src/main/java/com/example/flightradar65/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/flightradar65/ui/dashboard/DashboardFragment.java
@@ -14,12 +14,11 @@ import com.example.flightradar65.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+        private FragmentDashboardBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
