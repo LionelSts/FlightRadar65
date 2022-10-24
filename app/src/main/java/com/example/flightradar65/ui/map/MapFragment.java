95ab2f81e0cac309140ab7cba2e258b66fdb1e49
Lionel SMETS <lionel.smets@isen.yncrea.fr>
2022-10-24 15:22:14 +0200

    Map integrated

--- app/src/main/java/com/example/flightradar65/ui/map/MapFragment.java
+++ app/src/main/java/com/example/flightradar65/ui/map/MapFragment.java
@@ -4,11 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.flightradar65.databinding.FragmentMapBinding;
 
@@ -18,15 +16,10 @@ public class MapFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        MapViewModel mapViewModel =
-                new ViewModelProvider(this).get(MapViewModel.class);
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textMap;
-        mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
