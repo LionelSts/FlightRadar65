71f4527e92e090121239329cc2b496ffb9f55be8
Lionel SMETS <lionel.smets@student.junia.com>
2022-11-15 17:48:31 +0100

    Communicate login with other fragments

--- app/src/main/java/com/example/flightradar65/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/flightradar65/ui/account/AccountFragment.java
@@ -1,33 +1,42 @@
 package com.example.flightradar65.ui.account;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.flightradar65.databinding.FragmentAccountBinding;
+import com.example.flightradar65.R;
 
 public class AccountFragment extends Fragment {
-
-    private FragmentAccountBinding binding;
+    AccountViewModel accountViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AccountViewModel accountViewModel =
-                new ViewModelProvider(this).get(AccountViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+        Context context = requireActivity().getApplicationContext();
 
-        binding = FragmentAccountBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        Button loginButton = view.findViewById(R.id.loginButton);
+        EditText login = view.findViewById(R.id.editTextUsername);
+        EditText password = view.findViewById(R.id.editTextPassword);
+        Toast.makeText(context, loginButton.getText(), Toast.LENGTH_LONG).show();
+        loginButton.setOnClickListener(view1 -> {
+            if (!login.getText().toString().equals("") && !password.getText().toString().equals("")) {
+                accountViewModel.setLogin(String.valueOf(login.getText()));
+                Toast.makeText(context, "Login", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "error", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
--- app/src/main/java/com/example/flightradar65/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/flightradar65/ui/dashboard/DashboardFragment.java
@@ -1,32 +1,40 @@
 package com.example.flightradar65.ui.dashboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.flightradar65.databinding.FragmentDashboardBinding;
+import com.example.flightradar65.ui.account.AccountViewModel;
 
 public class DashboardFragment extends Fragment {
-
         private FragmentDashboardBinding binding;
+        private String userLogin;
 
         public View onCreateView(@NonNull LayoutInflater inflater,
                                  ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
+            Context context = requireActivity().getApplicationContext();
+            AccountViewModel viewModel = new ViewModelProvider(requireActivity()).get(AccountViewModel.class);
+            viewModel.getLogin().observe(getViewLifecycleOwner(), login -> userLogin = login);
+            binding = FragmentDashboardBinding.inflate(inflater, container, false);
+            View root = binding.getRoot();
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-    }
+            if (userLogin != null) {
+                String testVar = String.valueOf(viewModel.getLogin());
+                Toast.makeText(context, testVar, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "error", Toast.LENGTH_LONG).show();
+            }
+            return root;
+        }
 
     @Override
     public void onDestroyView() {
