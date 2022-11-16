76e3189bacbb3458ea7cee15acc7011f3561c8d0
augustin-m <60687546+augustin-m@users.noreply.github.com>
2022-11-16 15:28:43 +0100

    test login

--- app/src/main/java/com/example/flightradar65/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/flightradar65/ui/account/AccountFragment.java
@@ -1,7 +1,11 @@
 package com.example.flightradar65.ui.account;
 
+import static androidx.fragment.app.FragmentManager.TAG;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +18,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.flightradar65.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AccountFragment extends Fragment {
     AccountViewModel accountViewModel;
@@ -23,6 +31,8 @@ public class AccountFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
         View view = inflater.inflate(R.layout.fragment_account, container, false);
         accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
         Context context = requireActivity().getApplicationContext();
@@ -30,16 +40,54 @@ public class AccountFragment extends Fragment {
         Button loginButton = view.findViewById(R.id.loginButton);
         EditText login = view.findViewById(R.id.editTextUsername);
         EditText password = view.findViewById(R.id.editTextPassword);
-        Toast.makeText(context, loginButton.getText(), Toast.LENGTH_LONG).show();
+        //Toast.makeText(context, loginButton.getText(), Toast.LENGTH_LONG).show();
+
         loginButton.setOnClickListener(view1 -> {
             if (!login.getText().toString().equals("") && !password.getText().toString().equals("")) {
                 accountViewModel.setLogin(String.valueOf(login.getText()));
-                Toast.makeText(context, "Login", Toast.LENGTH_LONG).show();
+                //Toast.makeText(context, "Login", Toast.LENGTH_LONG).show();
+
+
+                mAuth.signInWithEmailAndPassword(login.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+
+                            @SuppressLint("RestrictedApi")
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(context, "Login successfully.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(context, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+                            }
+                        });
             } else {
                 Toast.makeText(context, "error", Toast.LENGTH_LONG).show();
             }
         });
         return view;
+    }
+
+    private void updateUI(FirebaseUser user) {
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+        }
     }
 
 }
